2bd5301503e6d8884f28f496ebd2d0f9b0ff4c56
Tyler Fisher <tfisher@politico.com>
2018-02-15 10:34:26 -0500

    install component apps

--- example/exampleapp/settings.py
+++ example/exampleapp/settings.py
@@ -19,6 +19,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'entity',
+    'geography',
+    'government',
+    'election',
+    'vote',
     'electionnight',
 ]
 
