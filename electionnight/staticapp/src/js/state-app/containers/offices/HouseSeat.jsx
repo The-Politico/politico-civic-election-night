d71243ee0240cdfdd35e2f313452451d26ece204
Jon McClure <jmcclure@politico.com>
2018-03-02 08:22:17 -0500

    district map

--- electionnight/staticapp/src/js/state-app/containers/offices/HouseSeat.jsx
+++ electionnight/staticapp/src/js/state-app/containers/offices/HouseSeat.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Table from './../results/Table';
+import DistrictMap from 'StateApp/components/DistrictMap';
 
 const HouseSeat = (props) => {
   const results = props.elections.map(election => (
     <Table election={election} {...props} />
   ));
-
   return (
     <section className='results-group'>
       <header>
         <h4 className='district'>
           {props.elections[0].office.label}
         </h4>
+        <DistrictMap
+          {...props}
+          district={props.elections[0].division.code}
+        />
       </header>
       <div className='container'>
         <div className='row'>
