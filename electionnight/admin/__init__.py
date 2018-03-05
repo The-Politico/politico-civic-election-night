4ff5b12022b597133823d4834b679e2e8c13f231
Tyler Fisher <tfisher@politico.com>
2018-03-05 14:32:03 -0500

    admin fixes

--- electionnight/admin/__init__.py
+++ electionnight/admin/__init__.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from electionnight.models import (APElectionMeta, PageContent, PageContentType,
                                 PageType, CandidateColorOrder)
+from vote.models import Votes
+
 from .page_content import PageContentAdmin
 from .color_order import CandidateColorOrderAdmin
+from .votes import VotesAdmin
 
 admin.site.register(APElectionMeta)
 
@@ -11,3 +14,5 @@ admin.site.register(PageContent, PageContentAdmin)
 admin.site.register(PageContentType)
 admin.site.register(PageType)
 admin.site.register(CandidateColorOrder, CandidateColorOrderAdmin)
+admin.site.unregister(Votes)
+admin.site.register(Votes, VotesAdmin)
