f3f4bc9621c014499078ffe4e16c1fbcf7367fa9
Tyler Fisher <tfisher@politico.com>
2018-04-17 17:49:59 -0400

    gross, elif case to handle staging

--- electionnight/views/specials.py
+++ electionnight/views/specials.py
@@ -178,7 +178,16 @@ class SpecialElectionPage(BaseView):
                     'https://www.politico.com/'
                     '/{}/district.json').format(geo),
             }
-
+        elif production and settings.AWS_S3_BUCKET != 'interactives.politico.com':
+            return {
+                'context': 'context.json',
+                'geo_county': (
+                    'https://s3.amazonaws.com/'
+                    'interactives.politico.com/{}/county.json').format(geo),
+                'geo_district': (
+                    'https://s3.amazonaws.com/'
+                    'interactives.politico.com/{}/district.json').format(geo),
+            }
         return {
             'context': reverse(
                 'electionnight_api_special-election-detail',
