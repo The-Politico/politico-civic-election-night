3feed92344f6b2d48c4912ddb5a27dc9d7ff6970
Tyler Fisher <tfisher@politico.com>
2018-05-01 17:19:49 -0400

    eslint

--- electionnight/staticapp/src/js/common/components/nav/LiveChatPromo.jsx
+++ electionnight/staticapp/src/js/common/components/nav/LiveChatPromo.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const LiveChatPromo = (props) => (
-  <a 
-    href="https://www.politico.com/election-results/2018/live-analysis/mar-20/"
-    target="_blank"
+  <a
+    href='https://www.politico.com/election-results/2018/live-analysis/may-08/'
+    target='_blank'
   >
     <div className='live-analysis'>
       <p>Live Analysis</p>
       <div className='dots'>
-          <span class='dot' />
-          <span class='dot' />
-          <span class='dot' />
-      </div>        
+        <span className='dot' />
+        <span className='dot' />
+        <span className='dot' />
+      </div>
     </div>
   </a>
 );
