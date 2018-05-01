26919c7d2cd8d83b9ea065a5d8e26dd58bbae1cf
Tyler Fisher <tfisher@politico.com>
2018-05-01 14:14:21 -0400

    bump to 0.2.0 for support for specials in generic pages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='politico-civic-election-night',
-    version='0.1.10',
+    version='0.2.0',
     description='',
     url='https://github.com/The-Politico/politico-civic-election-night',
     author='POLITICO interactive news',
