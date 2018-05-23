e6ce47be677ebeb5057373d2333080b9ecb8a483
Tyler Fisher <tfisher@politico.com>
2018-05-23 13:26:35 -0400

    automate link to live chat. closes #98

--- electionnight/staticapp/src/js/common/components/nav/LiveChatPromo.jsx
+++ electionnight/staticapp/src/js/common/components/nav/LiveChatPromo.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 
-const LiveChatPromo = (props) => (
-  <a
-    href='https://www.politico.com/election-results/2018/live-analysis/may-22/'
-    target='_blank'
-  >
-    <div className='live-analysis'>
-      <p>Live Analysis</p>
-      <div className='dots'>
-        <span className='dot' />
-        <span className='dot' />
-        <span className='dot' />
+const LiveChatPromo = (props) => {
+  const monthCrosswalk = {
+    '01': 'jan',
+    '02': 'feb',
+    '03': 'mar',
+    '04': 'apr',
+    '05': 'may',
+    '06': 'jun',
+    '07': 'jul',
+    '08': 'aug',
+    '09': 'sep',
+    '10': 'oct',
+    '11': 'nov',
+    '12': 'dec',
+  }
+
+  const chatDate = window.appConfig.electionSlug.split('-');
+  const month = monthCrosswalk[chatDate[1]];
+  const day = chatDate[2];
+
+  return (
+    <a
+      href={`https://www.politico.com/election-results/2018/live-analysis/${month}-${day}/`}
+      target='_blank'
+    >
+      <div className='live-analysis'>
+        <p>Live Analysis</p>
+        <div className='dots'>
+          <span className='dot' />
+          <span className='dot' />
+          <span className='dot' />
+        </div>
       </div>
-    </div>
-  </a>
-);
+    </a>
+  )
+};
 
 export default LiveChatPromo;
