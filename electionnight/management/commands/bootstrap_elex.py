00cce29d9b4fa4f8777f8cdb621319fb79114cd6
Tyler Fisher <tfisher@politico.com>
2018-06-07 13:44:48 -0400

    handle none of the candidates option, but poorly

--- electionnight/management/commands/bootstrap_elex.py
+++ electionnight/management/commands/bootstrap_elex.py
@@ -194,8 +194,13 @@ class Command(BaseCommand):
         """
         Gets or creates the Person object for the given row of AP data.
         """
+        if not row['first']:
+            first_name = 'None'
+        else:
+            first_name = row['first']
+
         person, created = entity.Person.objects.get_or_create(
-            first_name=row['first'],
+            first_name=first_name,
             last_name=row['last']
         )
 
