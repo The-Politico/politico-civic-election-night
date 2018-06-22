8326b2f6fd8a6b923ec0f29b41b78201cca8c351
Jon McClure <jmcclure@politico.com>
2018-06-22 09:03:26 -0400

    cleanup to module

--- electionnight/management/commands/bootstrap_results_db.py
+++ electionnight/management/commands/bootstrap_results_db.py
@@ -4,7 +4,6 @@ import sys
 from datetime import datetime
 from time import sleep
 
-from django.contrib.humanize.templatetags.humanize import ordinal
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
@@ -16,29 +15,7 @@ from geography.models import Division, DivisionLevel
 from tqdm import tqdm
 from vote.models import Votes
 
-
-def possessive_state(state_label):
-    if state_label[-1] == 's':
-        return '{}\''.format(state_label)
-    return '{}\'s'.format(state_label)
-
-
-def format_office_label(office, division_label):
-    """
-    Format the label for office into something we like for twitter.
-    """
-    if office.body:
-        if office.body.slug == 'senate':
-            return 'the Senate in {}'.format(division_label)
-        else:
-            return 'the House seat in {} {} District'.format(
-                possessive_state(division_label),
-                ordinal(office.division.code)
-            )
-    else:
-        # TODO: President
-        return 'the governor in {}'.format(division_label)
-    return office.label
+from .utils.notifications.formatters import format_office_label
 
 
 class Command(BaseCommand):
