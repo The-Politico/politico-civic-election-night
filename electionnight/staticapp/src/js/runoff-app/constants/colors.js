d6dd0b98d163a5c67757df140fc72c8c182d0756
Tyler Fisher <tfisher@politico.com>
2018-06-26 17:11:39 -0400

    fix colors

--- electionnight/staticapp/src/js/runoff-app/constants/colors.js
+++ electionnight/staticapp/src/js/runoff-app/constants/colors.js
@@ -1,8 +1,8 @@
 export const primaryColorsDem = {
   1: '#3571C0',
   2: '#D9C3A5',
-  3: '#9AD3FC',
-  4: '#8998AB',
+  3: '#8998AB',
+  4: '#9AD3FC',
   5: '#C3F4CD',
   6: '#5DAED4',
   7: '#8ABDC0',
@@ -13,13 +13,13 @@ export const primaryColorsDem = {
 
 export const primaryColorsGOP = {
   1: '#FD5E42',
-  2: '#CCBCA7',
-  3: '#B8948E',
-  4: '#FEE0C2',
-  5: '#FBC24E',
-  6: '#948064',
+  2: '#B8948E',
+  3: '#FBA723',
+  4: '#FFB2CB',
+  5: '#B41E61',
+  6: '#433E76',
   7: '#AC5141',
-  8: '#433E76',
-  9: '#B6ABE2',
-  10: '#EEABD0',
+  8: '#7957B4',
+  9: '#FEE0C2',
+  10: '#FD87F7',
 };
