ee625b589ccfe6bac7b5873018c3ee28d9123d3d
Tyler Fisher <tfisher@politico.com>
2018-06-26 09:40:08 -0400

    roll release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='politico-civic-election-night',
-    version='0.7.8',
+    version='0.7.9',
     description='',
     url='https://github.com/The-Politico/politico-civic-election-night',
     author='POLITICO interactive news',
