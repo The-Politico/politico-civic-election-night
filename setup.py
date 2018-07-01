24f0326ae43d5a7d93d48ca81a420dde65685fb5
Tyler Fisher <tfisher@politico.com>
2018-06-30 21:48:55 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='politico-civic-election-night',
-    version='0.8.3',
+    version='0.8.4',
     description='',
     url='https://github.com/The-Politico/politico-civic-election-night',
     author='POLITICO interactive news',
