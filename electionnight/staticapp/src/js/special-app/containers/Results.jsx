b751c865b26f6c8f22b704436ddf8a147d0ef9b7
Tyler Fisher <tfisher@politico.com>
2018-08-06 09:57:33 -0400

    0.10.1

--- electionnight/staticapp/src/js/special-app/containers/Results.jsx
+++ electionnight/staticapp/src/js/special-app/containers/Results.jsx
@@ -5,12 +5,14 @@ import House from './offices/House';
 import FetchRefresh from 'Common/components/FetchRefresh';
 import MarkdownText from 'Common/components/Markdowntext';
 import JumpLinks from 'Common/components/nav/JumpLinks';
+import LiveChatPromo from 'Common/components/nav/LiveChatPromo';
 
 const Results = (props) => {
   // We do some queries higher up so we don't need
   // to repeat them in sibling components...
   // ... Serialize candidates
   const {Candidate, Division, APMeta} = props.session;
+  const {stateResults} = props;
   const candidates = {};
   Candidate.all().toModelArray().forEach(candidate => {
     if (!candidate.id) return;
@@ -25,6 +27,13 @@ const Results = (props) => {
   props.state = Division
     .filter(d => d.level === DivisionLevels.state)
     .toModelArray();
+
+  props.resultsIn = false;
+  stateResults.forEach((result) => {
+    if (result.voteCount > 0) {
+      props.resultsIn = true;
+    }
+  });
 
   props.tabulated = true;
   APMeta.all().toModelArray().forEach(meta => {
@@ -43,6 +52,10 @@ const Results = (props) => {
     results.textContent = 'results';
   }
 
+  const liveChatPromo = props.resultsIn ? (
+    <LiveChatPromo />
+  ) : null;
+
   const fetchRefresh = props.tabulated ? null : (
     <FetchRefresh
       actions={props.actions}
@@ -53,6 +66,7 @@ const Results = (props) => {
   return (
     <div>
       {fetchRefresh}
+      {liveChatPromo}
       <StickyHeader {...props} />
       <MarkdownText
         content={props.content}
