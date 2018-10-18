dd1bac9128aeb007360d129dbeb39cc2a8f18577
Tyler Fisher <tfisher@politico.com>
2018-10-17 20:38:27 -0400

    don't call signals on management commands

--- electionnight/management/commands/bootstrap_electionnight_content.py
+++ electionnight/management/commands/bootstrap_electionnight_content.py
@@ -4,20 +4,20 @@ from geography.models import DivisionLevel
 from government.models import Jurisdiction
 from tqdm import tqdm
 
-from .methods import BootstrapContentMethods
+from electionnight.management.commands.methods import BootstrapContentMethods
 
 
 class Command(BaseCommand, BootstrapContentMethods):
     help = (
-        'Bootstraps page content items for pages for all elections on an '
-        'election day. Must be run AFTER bootstrap_election command.'
+        "Bootstraps page content items for pages for all elections on an "
+        "election day. Must be run AFTER bootstrap_election command."
     )
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'elections',
-            nargs='+',
-            help="Election dates to create content for."
+            "elections",
+            nargs="+",
+            help="Election dates to create content for.",
         )
 
     def route_election(self, election):
@@ -33,17 +33,18 @@ class Command(BaseCommand, BootstrapContentMethods):
 
     def handle(self, *args, **options):
         self.NATIONAL_LEVEL = DivisionLevel.objects.get(
-            name=DivisionLevel.COUNTRY)
-        self.STATE_LEVEL = DivisionLevel.objects.get(
-            name=DivisionLevel.STATE)
+            name=DivisionLevel.COUNTRY
+        )
+        self.STATE_LEVEL = DivisionLevel.objects.get(name=DivisionLevel.STATE)
         self.FEDERAL_JURISDICTION = Jurisdiction.objects.get(
-            division__level=self.NATIONAL_LEVEL)
-        print('Bootstrapping page content')
-        election_dates = options['elections']
+            division__level=self.NATIONAL_LEVEL
+        )
+        print("Bootstrapping page content")
+        election_dates = options["elections"]
 
         for election_date in election_dates:
-            print('> {}'.format(election_date))
+            print("> {}".format(election_date))
             election_day = ElectionDay.objects.get(date=election_date)
             for election in tqdm(election_day.elections.all()):
                 self.route_election(election)
-        print('Done.')
+        print("Done.")
